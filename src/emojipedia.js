38c91626015c170bd4e26257d960885e731df151
kp-63 <khushipadia28@gmail.com>
2023-09-08 00:21:23 +0530

    :tada: initial commit

---
+++ src/emojipedia.js
@@ -0,0 +1,32 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "😏",
+    name: "Smirking/Whimsical Face",
+    meaning:
+    "emoji expresses smugness, irony, coolness or playfulness. It is often used to convey flirtation or sexual innuendo, but use it with caution because sometimes someone might be up to something."
+  }
+];
+
+export default emojipedia;
